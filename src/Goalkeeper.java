8baff2d3f6c97ad3716bc1f3788c697b66e3e6fd
Ethan-Selvarajah2 <ethan.selvarajah25@ycdsbk12.ca>
2025-05-04 13:26:35 -0400

    Setup goalkeeper and outfield player classes fully and added their getters, but will add actual operation methods next.

--- src/Goalkeeper.java
+++ src/Goalkeeper.java
@@ -1,3 +1,18 @@
-public class Goalkeeper {
-    
+public class Goalkeeper extends Player {
+    private int saves;
+    private int goalsAllowed;
+
+    public Goalkeeper(String name, int age, String height, int weight, String nationality, int playerNumber, String position, int appearances, int substitutions, int assists, int foulsComitted, int foulsSuffered, int yellowCard, int redCard, int saves, int goalsAllowed) {
+        super(name, age, height, weight, nationality, playerNumber, position, appearances, substitutions, assists, foulsComitted, foulsSuffered, yellowCard, redCard);
+        this.saves = saves;
+        this.goalsAllowed = goalsAllowed;
+    }
+
+    public int getSaves() {
+        return saves;
+    }
+
+    public int getGoalsAllowed() {
+        return goalsAllowed;
+    }
 }
--- src/OutfieldPlayer.java
+++ src/OutfieldPlayer.java
@@ -1,3 +1,24 @@
-public class OutfieldPlayer {
-    
+public class OutfieldPlayer extends Player {
+    private int goals;
+    private int totalShots;
+    private int shotsOnTarget;
+
+    public OutfieldPlayer(String name, int age, String height, int weight, String nationality, int playerNumber, String position, int appearances, int substitutions, int assists, int foulsComitted, int foulsSuffered, int yellowCard, int redCard, int goals, int totalShots, int shotsOnTarget) {
+        super(name, age, height, weight, nationality, playerNumber, position, appearances, substitutions, assists, foulsComitted, foulsSuffered, yellowCard, redCard);
+        this.goals = goals;
+        this.totalShots = totalShots;
+        this.shotsOnTarget = shotsOnTarget;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public int getTotalShots() {
+        return totalShots;
+    }
+
+    public int getShotsOnTarget() {
+        return shotsOnTarget;
+    }
 }
