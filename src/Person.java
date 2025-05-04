b17e2cf6efec4d12e27f4ec3aa64dfe2d0918757
Ethan-Selvarajah2 <ethan.selvarajah25@ycdsbk12.ca>
2025-05-04 12:58:12 -0400

    Person class done.

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,35 @@
 public class Person {
-    private String
+    private String name;
+    private int age;
+    private String height;
+    private int weight;
+    private String nationality;
+
+    public Person(String name, int age, String height, int weight, String nationality) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.nationality = nationality;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
 }
