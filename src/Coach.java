ee44400c51fca6a0937eec5adddaf661544ee9f9
Ethan-Selvarajah2 <ethan.selvarajah25@ycdsbk12.ca>
2025-05-10 16:48:10 -0400

    Worked on coach class and created and added coach .csv file

--- src/Coach.java
+++ src/Coach.java
@@ -1,3 +1,34 @@
-public class Coach {
-    
+public class Coach extends Person {
+    private double term;
+    private String preferredFormation;
+    private int wins;
+    private int losses;
+
+    public Coach(String name, int age, String height, int weight, String nationality, double term, String preferredFormation, int wins, int losses) {
+        super(name, age, height, weight, nationality);
+        this.term = term;
+        this.preferredFormation = preferredFormation;
+        this.wins = wins;
+        this.losses = losses;
+    }
+
+    public double getTerm() {
+        return term;
+    }
+
+    public String getPreferredFormation() {
+        return preferredFormation;
+    }
+
+    public double getSuccessRate() {
+        if (wins + losses == 0) {
+            return 0.0;
+        } else {
+            return (double) wins / (wins + losses) * 100.0;
+        }
+    }
+
+    public String toString() {
+        return "Name: " + getName() + " | Position: Coach | Age: " + getAge() + " | Height: " + getHeight() + " | " + getWeight() + " lbs | Nationality: " + getNationality() + " | Term: " + getTerm() + " | Success Rate: " + getSuccessRate();
+    }
 }
